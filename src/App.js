7ef5b601dfc5149d52d67cd8c43e7ec1802b833a
Gabriel <gabriel.j.paixao@outlook.com>
2021-11-10 22:22:11 -0300

    Added basic expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -25,7 +26,8 @@ const App = () => {
 
   return (
     <div>
-      <Expenses expenses={expenses}></Expenses>
+      <NewExpense />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-const ExpenseItem = (props) =>  {
+const ExpenseItem = (props) => {  
+  //Properties
   const expense = props.expense;
+  const [title, setTitle] = useState(expense.title);
+
+  //Functions
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
+  //Html
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
-      </div>      
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
